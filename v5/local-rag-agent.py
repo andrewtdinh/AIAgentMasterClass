18795197d4b1423d63ad45cec1dca08a30eae6e2
Andrew Dinh <andrewdinh74@yahoo.com>
2025-04-10 14:54:17 -0700

    Add get_local_model function

--- v5/local-rag-agent.py
+++ v5/local-rag-agent.py
@@ -15,6 +15,26 @@ load_dotenv()
 model = os.getenv('LLM_MODEL', 'meta-llama/Meta-Llama-3.1-405B-Instruct')
 rag_directory = os.getenv('DIRECTORY', 'meeting_notes')
 
+@st.cache_resource
+def get_local_model():
+  return HuggingFaceEndpoint(
+    repo_id=model,
+    task="text-generation",
+    max_new_tokens=1024,
+    do_sample=False
+  )
+
+  # If you want to run the model absolutely locally - VERY resource intense!
+  # return HuggingFacePipeline.from_model_id(
+  #     model_id=model,
+  #     task="text-generation",
+  #     pipeline_kwargs={
+  #         "max_new_tokens": 1024,
+  #         "top_k": 50,
+  #         "temperature": 0.4
+  #     },
+  # )
+
 def main():
   st.title("Chat with Local Documents")
 
