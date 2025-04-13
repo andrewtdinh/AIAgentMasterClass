0e3a8e7b624c1f39dce70225e00b59be505dd6e6
Andrew Dinh <andrewdinh74@yahoo.com>
2025-04-13 16:41:28 -0700

    Update dependencies to resolve conflicts

--- v6/rag-document-loader.py
+++ v6/rag-document-loader.py
@@ -1,4 +1,5 @@
-from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+# from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_chroma import Chroma
@@ -27,7 +28,7 @@ def main():
     docs = load_documents(rag_directory)
 
     # Create the open-source embedding function
-    embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+    embedding_function = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
     # Load the documents into Chroma and save it to the disk
     Chroma.from_documents(docs, embedding_function, persist_directory="./chroma_db")
